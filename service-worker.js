b5442e74c0abc4e0a51f9c15310617e3c6d9fc4b
ahmadok12 <ahmad.ok12@gmail.com>
2026-08-22 22:10:37 +0500

    Enlarge attachment thumbnails and reduce filename size

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'scs-inquiry-v3';
+const CACHE = 'scs-inquiry-v4';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest', './assets/icon.svg', './assets/icon-192.png', './assets/icon-512.png'];
 
 self.addEventListener('install', event => {
