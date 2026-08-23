42ed7680b1da66d3b5ee3e8c1509842e8bb00e2c
ahmadok12 <ahmad.ok12@gmail.com>
2026-08-23 09:25:01 +0500

    Add tappable product recognition photos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'scs-inquiry-v7';
+const CACHE = 'scs-inquiry-v8';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest', './assets/icon.svg', './assets/icon-192.png', './assets/icon-512.png'];
 
 self.addEventListener('install', event => {
